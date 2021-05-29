af1ec8f1714c31f1237191897cb666874b99aa4d
Michael Streeter <michael.streeter@logdna.com>
2021-05-29 01:38:45 -0500

    MS: initial elixir app

---
+++ lib/fight_fair.ex
@@ -0,0 +1,18 @@
+defmodule FightFair do
+  @moduledoc """
+  Documentation for `FightFair`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FightFair.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fight_fair_test.exs
@@ -0,0 +1,8 @@
+defmodule FightFairTest do
+  use ExUnit.Case
+  doctest FightFair
+
+  test "greets the world" do
+    assert FightFair.hello() == :world
+  end
+end
