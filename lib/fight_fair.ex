6a50dcc1c95d956c3f14329293a6eea555a0dfc7
Michael Streeter <michael.streeter@logdna.com>
2021-06-25 12:03:29 -0700

    MS: added start_fight function

--- lib/fight_fair.ex
+++ lib/fight_fair.ex
@@ -1,9 +1,20 @@
 defmodule FightFair do
-  def hello do
-    :world
-  end
+  alias FightFair.Db.{Fight, User}
 
   def migrate do
     FightFair.Release.migrate()
+  end
+
+  def start_fight(user_id, partner_id, subject) do
+    user = User.get_by_id(user_id)
+    partner = User.get_by_id(partner_id)
+
+    {:ok, fight} =
+      Fight.insert(%{
+        users: [user, partner],
+        subject: subject
+      })
+
+    fight
   end
 end
--- test/fight_fair_test.exs
+++ test/fight_fair_test.exs
@@ -1,8 +1,25 @@
 defmodule FightFairTest do
-  use ExUnit.Case
+  use FightFair.RepoCase
+  alias FightFair.Repo
+  alias FightFair.Db.{Action, Fight, User}
+
   doctest FightFair
 
-  test "greets the world" do
-    assert FightFair.hello() == :world
+  @user %User{name: "michael", email: "eMMe@gmail.com"}
+  @partner %User{name: "lara", email: "l@gmail.com"}
+  @action %Action{name: "started_fight"}
+
+
+  test "start a fight" do
+    user = Repo.insert!(@user)
+    partner = Repo.insert!(@partner)
+    subject = "dude, stop"
+
+    IO.inspect(user.id)
+    IO.inspect(partner.id)
+
+    fight = FightFair.start_fight(user.id, partner.id, subject)
+
+    assert fight.id > 0
   end
 end
