9e124b8b7397f9cf2340860a74da5c55526f8347
Michael Streeter <michael.streeter@logdna.com>
2022-06-19 19:46:18 -0500

    MS: create user now has user tokens and hashed_passwords

--- priv/repo/migrations/20210529065604_create_user.exs
+++ priv/repo/migrations/20210529065604_create_user.exs
@@ -2,12 +2,27 @@ defmodule FightFair.Repo.Migrations.CreateUser do
   use Ecto.Migration
 
   def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext", ""
+
     create table(:user) do
       add :name, :string
-      add :email, :string
+      add :email, :citext, null: false
+      add :hashed_password, :string #, null: false << need to fix this >>
+      add :confirmed_at, :naive_datetime
       timestamps()
     end
 
     create unique_index(:user, [:email])
+
+    create table(:user_tokens) do
+      add :user_id, references(:user, on_delete: :delete_all), null: false
+      add :token, :binary, null: false
+      add :context, :string, null: false
+      add :sent_to, :string
+      timestamps(updated_at: false)
+    end
+
+    create index(:user_tokens, [:user_id])
+    create unique_index(:user_tokens, [:context, :token])
   end
 end
